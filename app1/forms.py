2d57a6fa211e54778ffaba6480e3d101072efc31
mp <mp@necropolis>
2023-12-08 22:47:18 +0200

    fix templates

---
+++ app1/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from app1.models import VeteransAssistant, Region, TypeOfUser
+
+
+class UsersTGForm(forms.Form):
+    full_name = forms.CharField(max_length=255, required=False)
+    phone_number = forms.CharField(max_length=255, required=False)
+    date_of_birth = forms.DateField(required=False)
+    region = forms.ModelChoiceField(queryset=Region.objects.all(), required=False)
+    type_of_user = forms.ModelChoiceField(queryset=TypeOfUser.objects.all(), required=False)
+    time_create = forms.DateTimeField(required=False, widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+
+
+class QuestionForm(forms.Form):
+    veterans_assistant = forms.ModelChoiceField(queryset=VeteransAssistant.objects.all(), required=False)
+    user_gt = forms.CharField(max_length=255, required=False)
+    text_question = forms.CharField(max_length=255, required=False)
+    time_create = forms.DateTimeField(required=False, widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+
